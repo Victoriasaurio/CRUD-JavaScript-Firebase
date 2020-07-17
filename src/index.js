fc54c9e7de7be4fc896044e6f5aa0fc592c28cbd
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-07-16 23:19:11 -0500

    Funcionalidad de los botones Delete y Edit

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ const getTask = () => db.collection('tasks').get();
 //Función que se maneja siempre que exista cambios nuevos en la BD
 const onGetTasks = (callback) => db.collection('tasks').onSnapshot(callback);
 
+//Elimina la tarea de la colección
+const deleteTask = id => db.collection('tasks').doc(id).delete();
+
+//Obteniedo una colección de la base de datos por medio del ID
+const getTaskId = id => db.collection('tasks').doc(id).get();
+
 window.addEventListener('DOMContentLoaded', async(e) => {
     //const querySnapshot = await getTask(); //Obtiene los datos de la BD siempre que se actualice la página.
     //querySnapshot -> Contiene toda la colección de datos de la BD
@@ -27,15 +33,35 @@ window.addEventListener('DOMContentLoaded', async(e) => {
             console.log(doc.data()); //Muestra la colección de datos de manera legible
 
             const task = doc.data();
+            task.id = doc.id; //Obtiene el ID de la colección
+            console.log(task.id);
 
             taskContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
         <h5>${task.title}</h5>
         <p>${task.description}</p>
         <div>
-            <button class="btn btn-danger">Delete</button>
-            <button class="btn btn-secondary">Edit</button>
+            <button class="btn btn-danger btn-delete" data-id="${task.id}">Delete</button>
+            <button class="btn btn-secondary btn-edit" data-id="${task.id}">Edit</button>
         </div>
-        </div>`
+        </div>`;
+
+            //Seleccionando todos los botones y escuchando cada uno por separado con la propiedad click
+            const btnDelete = document.querySelectorAll('.btn-delete');
+            btnDelete.forEach(btn => {
+                btn.addEventListener('click', async(e) => {
+                    //console.log(e.target.dataset.id); //Obtiene el ID de la colección siempre que se presione el botón Delete.
+                    await deleteTask(e.target.dataset.id); //Elimina las colecciones uno por uno
+                })
+            })
+
+            //Permite editar los valores
+            const btnEdit = document.querySelectorAll('.btn-edit');
+            btnEdit.forEach(btn => {
+                btn.addEventListener('click', async(e) => {
+                    const doc = await getTaskId(e.target.dataset.id);
+                    console.log(doc.data());
+                })
+            })
         }); //Recorre el objeto dado por firebase 
     });
 });
